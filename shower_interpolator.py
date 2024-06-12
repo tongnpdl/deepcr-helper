e7db56f9707c22d953abe82cc5db05ccc2810b2e
npunsuebsay <tongnpdl@gmail.com>
2024-06-12 11:37:03 -0500

    add descriptions and expected behavior with simple structure

--- shower_interpolator.py
+++ shower_interpolator.py
@@ -1,2 +1,20 @@
 import numpy as np
 
+
+class interp_shower2d:
+	references = []
+	def __init__(self,library=None):
+		"""
+		Initialize a callable interpolator object.
+		The object lookup library of simulated cosmic ray showers for appropriate entries to be used for interpolation.
+		"""
+		if library is None: ## use example library
+			pass ## for now
+
+
+	def __call__(self,x,y,method='nearest_neighbor'):
+		"""
+		Call function return interpolated electric fields
+		"""
+		t,Ex,Ey,Ez = np.array([0,0,0,0],dtype=float)
+		return t,Ex,Ey,Ez
